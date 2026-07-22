fa225f9e74fafdb98d696eecd0b96ad3ac075792
Vladimir Fomin <vfomin@art3d.ru>
2026-07-22 12:59:04 +0300

    New version

--- src/PhpAppCore.php
+++ src/PhpAppCore.php
@@ -4,6 +4,9 @@ namespace Beenlord\PhpAppCore;
 
 class PhpAppCore {
     static function example() {
-        echo 'Hello world!';
+	    echo 'Hello world!';
+	    echo 'Hello world!';
+	    echo 'Hello world!';
+	    echo 'Hello world!';
     }
 }
